ec2086bbf9455397c68bdb0eb5010bd97394cf0b
mayank7463 <mayank18091999@gmail.com>
2024-06-25 00:48:00 +0530

    second commit

--- src/pages/Overview/MajorHighlights.jsx
+++ src/pages/Overview/MajorHighlights.jsx
@@ -3,11 +3,11 @@ import './MajorHighlights.css'
 
 export default function MajorHighlights() {
   return (
-   <>
-     <div className=' bg-gradient-to-r from-[#026E89] to-[#005067]'>
+   <div className='p-8 bg-gradient-to-r from-[#026E89] to-[#005067]'>
+     <div>
      <h1 className="flex justify-center text-6xl mb-8 mt-8 text-white font-bold">Major Highlights of FY24</h1>
      <div className="flex justify-evenly space-x-12 items-center b">
-        <div className="grid lg:grid-cols-3 sm:grid-cols-1 gap-16 w-full max-w-screen-lg mt-2 mb-4">
+        <div className="grid lg:grid-cols-5 sm:grid-cols-1  w-full  mt-2 mb-4">
             <div>
                 <p className="border-highlights-1">13+</p>
                 <p className="border-highlights-2 ">Years in existence</p>
@@ -20,10 +20,6 @@ export default function MajorHighlights() {
                 <p className=" border-highlights-1">16</p>
                 <p className="text-xl border-highlights-2">States</p>
             </div>
-        </div>
-    </div>
-    <div className="flex flex-col justify-start items-center">
-        <div className="grid lg:grid-cols-3 sm:grid-cols-1 gap-16 w-full max-w-screen-lg mt-2 mb-4">
             <div>
                 <p className="border-highlights-1">AA+/Stable</p>
                 <p className="border-highlights-2 ">Credit Rating</p>
@@ -32,14 +28,15 @@ export default function MajorHighlights() {
                 <p className="border-highlights-1">3,232</p>
                 <p className="border-highlights-2">Personnel</p>
             </div>
+            
+        </div>
+    </div>
+    <div className="flex flex-col justify-start items-center">
+        <div className="grid lg:grid-cols-5 sm:grid-cols-1 gap-16 w-full mt-2 mb-4">
             <div >
                 <p className=" border-highlights-1">2,10,000+</p>
                 <p className="text-xl border-highlights-2">Customers served</p>
             </div>
-        </div>
-    </div>
-    <div className="flex flex-col justify-start items-center">
-        <div className="grid lg:grid-cols-3 sm:grid-cols-1 gap-16 w-full max-w-screen-lg mt-2 mb-4">
             <div>
                 <p className="border-highlights-1">	<span>&#8377;</span>13,762<sub>crore</sub></p>
                 <p className="border-highlights-2 ">Asset Under
@@ -58,10 +55,15 @@ export default function MajorHighlights() {
                 <p className="text-xl border-highlights-2">Total Revenue</p>
                 <p className='text-[#fff]'><span className='text-2xl font-[500]'>82.4% </span>Y-o-Y growth</p>
             </div>
+            <div>
+                <p className="border-highlights-1"><span>&#8377;</span>217.40<sub>crore</sub></p>
+                <p className="border-highlights-2 ">Profit after Tax for FY 24</p>
+                <p className='text-[#fff]'><span className='text-2xl font-[500]'>58% </span>Y-o-Y growth</p>
+            </div>
         </div>
     </div>
     <div className="flex flex-col justify-start items-center">
-        <div className="grid lg:grid-cols-3 sm:grid-cols-1 gap-16 w-full max-w-screen-lg mt-2 mb-4">
+        <div className="grid lg:grid-cols-5 sm:grid-cols-1 gap-16 w-full  mt-2 mb-4">
             <div>
                 <p className="border-highlights-1"><span>&#8377;</span>217.40<sub>crore</sub></p>
                 <p className="border-highlights-2 ">Profit after Tax for FY 24</p>
@@ -76,10 +78,6 @@ export default function MajorHighlights() {
                 <p className=" border-highlights-1">3rd largest</p>
                 <pre className="text-xl border-highlights-2">Affordable Housing <br />Finance company in India</pre>
             </div>
-        </div>
-    </div>
-    <div className="flex flex-col justify-start items-center">
-        <div className="grid lg:grid-cols-3 sm:grid-cols-1 gap-16 w-full max-w-screen-lg mt-2 mb-4">
             <div>
                 <p className="border-highlights-1">Fastest</p>
                 <pre className="border-highlights-2 ">growing AHFC in the <br />
@@ -90,15 +88,16 @@ export default function MajorHighlights() {
                 <pre className="border-highlights-2">leadership team driving <br />
                 quality business growth</pre>
             </div>
+        </div>
+    </div>
+    <div className="flex flex-col justify-start items-center">
+        <div className="grid lg:grid-cols-5 sm:grid-cols-1 gap-16 w-full  mt-2 mb-4">
+            
             <div >
                 <p className=" border-highlights-1">Dominant</p>
                 <pre className="text-xl border-highlights-2">in Gujarat, Tamil Nadu, Andhra <br />
                 Pradesh, Telangana and Karnataka</pre>
             </div>
-        </div>
-    </div>
-    <div className="flex flex-col justify-start items-center">
-        <div className="grid lg:grid-cols-2 sm:grid-cols-1 gap-16 w-full max-w-screen-lg mt-2 mb-4 space-x-20">
             <div>
                 <p className="border-highlights-1">Great Place
                 to Work</p>
@@ -113,6 +112,6 @@ export default function MajorHighlights() {
     </div>
     </div>
 
-   </>
+   </div>
   )
 }
